8d551b9f96284fbdc2525febd74a9f454e0a3e85
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 14:19:04 -0700

    Add standard deviation calculation.

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -3,3 +3,25 @@ ages = [24, 30, 18, 20, 41]
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
+
+sum = ages.sum
+count = ages.count
+average = (sum.to_f/count)
+
+subtract_each = []
+
+ages.each do |age|
+  subtract_each << (age - average)
+end
+
+square_each = []
+
+subtract_each.each do |subtraction|
+  square_each << (subtraction ** 2)
+end
+
+sum_of_squared = square_each.sum
+squared_divided_by_count = sum_of_squared/count
+
+
+puts Math.sqrt(squared_divided_by_count)
