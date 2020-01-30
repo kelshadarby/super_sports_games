e3388d6d8ca02d00f66749fb4b5e1bda3cbf1191
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 16:05:06 -0700

    Add attr_reader test

--- test/event_test.rb
+++ test/event_test.rb
@@ -10,5 +10,10 @@ class EventTest < Minitest::Test
 
     assert_instance_of Event, event
   end
+  def test_it_has_name
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+
+    assert_equal "Curling", event.name
+  end
 
 end # Class
