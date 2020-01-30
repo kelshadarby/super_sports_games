4a9d31470f874c6ecff55063632f6b5963b6b6b6
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 16:03:26 -0700

    Add initialize test

---
+++ test/event_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/games'
+require './lib/event'
+
+class EventTest < Minitest::Test
+
+  def test_it_exists
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+
+    assert_instance_of Event, event
+  end
+
+end # Class
