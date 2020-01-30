5fb245e98bc68eb168c831ef89e18ab539cec5be
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 16:07:26 -0700

    Add second attr_reader test

--- test/event_test.rb
+++ test/event_test.rb
@@ -16,4 +16,10 @@ class EventTest < Minitest::Test
     assert_equal "Curling", event.name
   end
 
+  def test_it_has_ages
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+
+    assert_equal [24, 30, 18, 20, 41], event.ages
+  end
+
 end # Class
